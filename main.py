ca0e3ea5687c709fa46f64e78c773bdaaa797aee
molamoh <iat@protocol.com>
2026-04-24 22:00:52 +0200

    Clean project structure + move API to iat/api

---
+++ main.py
@@ -0,0 +1,3 @@
+from iat.api.agent_b_api import app
+
+# Entry point for deployment
--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-uvicorn agent_b_api:app --host 0.0.0.0 --port $PORT
+uvicorn main:app --host 0.0.0.0 --port $PORT
